84d772a026c397757e5bec0d0e03f7d155f90a36
Segun <miksegun1996@gmail.com>
2023-09-05 00:54:58 +0100

    new repo for the admin

---
+++ app/Http/Controllers/Api/V1/ConfigController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+use App\Models\Zone;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
+use App\CentralLogics\Helpers;
+use Grimzy\LaravelMysqlSpatial\Types\Point;
+
+
+class ConfigController extends Controller
+{
+        public function geocode_api(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'lat' => 'required',
+            'lng' => 'required',
+        ]);
+
+        if ($validator->errors()->count()>0) {
+            return response()->json(['errors' => Helpers::error_processor($validator)], 403);
+        }
+       
+        $response = Http::get('https://maps.googleapis.com/maps/api/geocode/json?latlng='.$request->lat.','.$request->lng.'&key='."AIzaSyAwph5iP61uHokKAovxY2PT3TuvMjJkFzE");
+        return $response->json();
+    }
+        public function get_zone(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'lat' => 'required',
+            'lng' => 'required',
+        ]);
+
+        if ($validator->errors()->count()>0) {
+            return response()->json(['errors' => Helpers::error_processor($validator)], 403);
+        }
+        $point = new Point($request->lat,$request->lng);
+        $zones = Zone::whereRaw("ST_Contains(`coordinates`, ST_GeomFromText('POINT($point)', 0))")
+        ->latest()
+        ->get();
+        // $zones = Zone::contains('coordinates', $point)->latest()->get();
+       /* if(count($zones)<1)
+        {
+            return response()->json(['message'=>trans('messages.service_not_available_in_this_area_now')], 404);
+        }
+        foreach($zones as $zone)
+        {
+            if($zone->status)
+            {
+                return response()->json(['zone_id'=>$zone->id], 200);
+            }
+        }*/
+        //return response()->json(['message'=>trans('messages.we_are_temporarily_unavailable_in_this_area')], 403);
+         return response()->json(['zone_id'=>1], 200);
+    }
+
+
+    //     public function configuration() 
+    // {
+    //     return response()->json([
+            
+    //         'business_name' => BusinessSetting::where(['key' => 'business_name'])->first()->value,
+
+    //         'base_urls' => [
+    //             'customer_image_url' => asset('storage/profile'),
+    //             'business_logo_url' => asset('storage/business'),
+    //         ],
+
+    //         'country' => BusinessSetting::where(['key' => 'country'])->first()->value,
+    //         'default_location' => ['lat'=>'23.75343', 'lng'=>'90.36043'],
+    //     ]);
+    // }
+
+
+        public function place_api_autocomplete(Request $request)
+        {
+            $validator = Validator::make($request-> all(), [
+                'search_text' => 'required',
+            ]);
+
+            if ($validator->errors()->count()>0){
+                return response()->json(
+                    ['errors' => Helpers::error_processor($validator)],
+                    403);
+            }
+
+            $response = Http::get(
+                'https://maps.googleapis.com/maps/api/place/autocomplete/json?input='.$request['search_text'].'&key='."AIzaSyAwph5iP61uHokKAovxY2PT3TuvMjJkFzE"
+            );
+
+            return $response->json();
+        }
+}
